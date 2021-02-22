a1ab4160ad4d057ec4bb1426e2fec93e1171b160
Dmitry Semenov <dmitry@saritasa.com>
2021-02-22 12:39:21 -0800

    feat: add notifications.sh

---
+++ notifications.sh
@@ -0,0 +1,17 @@
+# small public function printing message in a box
+# usage: header [message]
+# example: header "header world"
+
+function header() {
+    message=$1
+    message=${message^^}
+    message_size=${#message}
+
+    tput setaf 1; tput bold;
+    echo "
+    ⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡
+    💬 $message
+    ⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡"
+    tput sgr0
+    echo
+}
